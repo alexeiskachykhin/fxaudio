7018bedf58eceb8586829e120c620a9fa5644365
Alexei Skachykhin <alexei.skachykhin@live.com>
2013-09-29 17:48:28 +0300

    Introduced FxSignalMergerUnit and corresponding unit-test.

---
+++ Sources/Cell/Cell.AudioEngine/sources/units/fxSignalMergerUnit.ts
@@ -0,0 +1,31 @@
+﻿/// <reference path="../_references.ts" />
+
+
+module FxAudioEngine.Units {
+    'use strict';
+
+
+    export class FxSignalMergerUnit extends FxUnit<FxSignalMergerUnitCircuit> {
+
+        constructor(context: FxUnitContext, numberOfInputs: number = 6) {
+            super(new FxSignalMergerUnitCircuit(context, numberOfInputs));
+        }
+    }
+
+
+    export class FxSignalMergerUnitCircuit extends FxAdapterUnitCircuit<GainNode> {
+
+        constructor(context: FxUnitContext, numberOfInputs: number) {
+            super(context, NodeType.GAIN);
+
+            this._connectInputs(numberOfInputs);
+        }
+
+
+        private _connectInputs(numberOfInputs: number): void {
+            for (var i = this.inputs.length - 1; i < numberOfInputs; i++) {
+                this._addInputNode(this.audioNode);
+            }
+        }
+    }
+}
