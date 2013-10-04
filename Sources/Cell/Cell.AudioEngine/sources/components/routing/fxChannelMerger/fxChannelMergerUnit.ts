a52263b42cc1067f68367002008ae04c93f594aa
Alexei Skachykhin <alexei.skachykhin@live.com>
2013-10-05 01:28:35 +0300

    Components have been decomposed into categories.

---
+++ Sources/Cell/Cell.AudioEngine/sources/components/routing/fxChannelMerger/fxChannelMergerUnit.ts
@@ -0,0 +1,14 @@
+﻿/// <reference path="../../../_references.ts" />
+
+
+module FxAudioEngine {
+    'use strict';
+
+
+    export class FxChannelMergerUnit extends FxUnit<FxChannelMergerCircuit> {
+
+        constructor(context: FxContext, numberOfInputs: number = 6) {
+            super(new FxChannelMergerCircuit(context, numberOfInputs));
+        }
+    }
+}
