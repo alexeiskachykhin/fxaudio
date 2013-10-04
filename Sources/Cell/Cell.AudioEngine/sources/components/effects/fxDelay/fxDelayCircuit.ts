18d1e8a74ed68a8ff0a28440ebec9c10d6433004
Alexei Skachykhin <alexei.skachykhin@live.com>
2013-10-05 01:28:35 +0300

    Components have been decomposed into categories.

---
+++ Sources/Cell/Cell.AudioEngine/sources/components/effects/fxDelay/fxDelayCircuit.ts
@@ -0,0 +1,14 @@
+﻿/// <reference path="../../../_references.ts" />
+
+
+module FxAudioEngine {
+    'use strict';
+
+
+    export class FxDelayCircuit extends FxAdapterCircuit<DelayNode> {
+
+        constructor(context: FxContext, maxDelayTime: number) {
+            super(context, NodeType.DELAY, maxDelayTime);
+        }
+    }
+}
---
+++ Sources/Cell/Cell.AudioEngine/sources/components/routing/fxSignalMerger/fxSignalMergerUnit.ts
@@ -0,0 +1,14 @@
+﻿/// <reference path="../../../_references.ts" />
+
+
+module FxAudioEngine {
+    'use strict';
+
+
+    export class FxSignalMergerUnit extends FxUnit<FxSignalMergerCircuit> {
+
+        constructor(context: FxContext, numberOfInputs: number = 6) {
+            super(new FxSignalMergerCircuit(context, numberOfInputs));
+        }
+    }
+}
