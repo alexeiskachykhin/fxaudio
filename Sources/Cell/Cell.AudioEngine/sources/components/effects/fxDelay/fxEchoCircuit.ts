0f98d5f99f546471f2e0688a6d65da2dca7b1ac2
Alexei Kostevich <alexei.kostevich@live.com>
2013-10-07 02:54:01 +0300

    Introduced FxEchoUnit and corresponding unit-test.

---
+++ Sources/Cell/Cell.AudioEngine/sources/components/effects/fxDelay/fxEchoCircuit.ts
@@ -0,0 +1,73 @@
+/// <reference path="../../../_references.ts" />
+
+
+module FxAudioEngine {
+    'use strict';
+
+
+    export class FxEchoCircuit extends FxCircuit {
+
+        private _inputGainNode: GainNode;
+
+        private _delayNode: DelayNode;
+
+        private _feedbackGainNode: GainNode;
+
+        private _echoGainNode: GainNode;
+
+        private _outputGainNode: GainNode;
+
+
+        public get inputGainNode(): GainNode {
+            return this._inputGainNode;
+        }
+
+        public get delayNode(): DelayNode {
+            return this._delayNode;
+        }
+
+        public get feedbackGainNode(): GainNode {
+            return this._feedbackGainNode;
+        }
+
+        public get echoGainNode(): GainNode {
+            return this._echoGainNode;
+        }
+
+        public get outputGainNode(): GainNode {
+            return this._outputGainNode;
+        }
+
+
+        constructor(context: FxContext, maxDelayTime) {
+            super(context);
+
+            this._buildAudioCircuit(maxDelayTime);
+        }
+
+
+        private _buildAudioCircuit(maxDelayTime): void {
+            var audioContext: AudioContext = this.context.audioContext;
+
+            this._inputGainNode = audioContext.createGain();
+            this._delayNode = audioContext.createDelay(maxDelayTime);
+            this._feedbackGainNode = audioContext.createGain();
+            this._echoGainNode = audioContext.createGain();
+            this._outputGainNode = audioContext.createGain();
+
+            var linearRoute: AudioNode[] = [
+                this._inputGainNode,
+                this._delayNode,
+                this._echoGainNode,
+                this._outputGainNode
+            ];
+
+            this._inputGainNode.connect(this._outputGainNode);
+            FxAudioUtilities.WebAudioAPI.routeLinear(linearRoute);
+            FxAudioUtilities.WebAudioAPI.routeWithFeedback(this._delayNode, this._feedbackGainNode);
+
+            this._publishInputComponent(this._inputGainNode);
+            this._publishOutputComponent(this._outputGainNode);
+        }
+    }
+}
---
+++ Sources/Cell/Cell.AudioEngine/tests/integration/live-input-source-echo-unit/app.js
@@ -0,0 +1,36 @@
+﻿(function () {
+    'use strict';
+
+
+    function getInputStream(callback) {
+        navigator.getUserMedia({
+            audio: true
+        }, callback, function () { });
+    }
+
+
+    getInputStream(function (stream) {
+        var context = new FxAudioEngine.FxRealTimeContext();
+
+        var sourceUnit = new FxAudioEngine.FxLiveInputSourceUnit(context);
+        var destinationUnit = new FxAudioEngine.FxAudioDestinationUnit(context);
+        var echoUnit = new FxAudioEngine.FxEchoUnit(context);
+
+        echoUnit.delayTime = 3;
+        echoUnit.feedbackGain = 0.25;
+        echoUnit.echoGain = 0.5;
+
+        sourceUnit.ports.outputs[0].connect(echoUnit.ports.inputs[0]);
+        echoUnit.ports.outputs[0].connect(destinationUnit.ports.inputs[0]);
+
+        var initOperation = sourceUnit.init(stream);
+
+        initOperation.addEventListener('success', function () {
+            sourceUnit.stream.start(0);
+        });
+
+        initOperation.addEventListener('error', function () {
+            throw new Error('Initialization of source node has failed.');
+        });
+    });
+}());
