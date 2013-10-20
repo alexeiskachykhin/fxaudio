7fff9e7bc796eda5bb6e3e79ddff6164e3bda6db
Alexei Skachykhin <alexei.skachykhin@live.com>
2013-10-20 03:56:13 +0300

    Introduced Chorus and corresponding unit-tests.

---
+++ Sources/Cell/Cell.AudioEngine/sources/components/effects/chorus/chorusCircuit.ts
@@ -0,0 +1,23 @@
+/// <reference path="../../../_references.ts" />
+
+
+module FxAudioEngine {
+    'use strict';
+
+
+    var DEFAULT_FEEDBACK = 0;
+    var DEFAULT_DELAY_TIME = 0.010;
+
+
+    export class ChorusCircuit extends ModulatedComboFilterCircuit {
+
+        constructor(context: Context) {
+            Contract.isNotNullOrUndefined(context, 'context');
+
+            super(context);
+
+            this.feedbackNode.gain.value = DEFAULT_FEEDBACK;
+            this.delayNode.delayTime.value = DEFAULT_DELAY_TIME;
+        }
+    }
+}
